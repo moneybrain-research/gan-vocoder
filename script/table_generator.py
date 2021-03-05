4f4fa3709187240b3e07e333e4ffe5264ed04c96
luaperl <ngh3053@gmail.com>
2021-03-05 15:18:15 +0900

    Audio url fix

--- script/table_generator.py
+++ script/table_generator.py
@@ -8,9 +8,10 @@ for category in ['gt', 'tt']:
         row = []
         for model in models:
             if model == '_gt':
-                row.append(f'<audio src="assets/audio/{model}/{wavfile}" controls preload="auto">')
+                row.append(f'<audio src="https://raw.githubusercontent.com/moneybrain-research/gan-vocoder/master/assets/audio/_gt/{wavfile}" controls preload="auto">')
             else:
-                row.append(f'<audio src="assets/audio/{model}/{category}/{wavfile}" controls preload="auto">')
+                row.append(f'<audio src="https://raw.githubusercontent.com/moneybrain-research/gan-vocoder/master/assets/audio/{model}/{category}/{wavfile}" controls preload="auto">')
+                # row.append(f'<audio src="assets/audio/{model}/{category}/{wavfile}" controls preload="auto">')
 
         row = '|' + '|'.join(row) + '|'
         print(row)
